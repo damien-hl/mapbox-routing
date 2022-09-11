01495386f13e4b86ab3f4aad97bc3a1a9be9e96c
Damien Heulin <damienheulin87@gmail.com>
2022-09-11 09:18:42 +0200

    chore: published to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mapbox-routing/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          mapbox: ["mapbox-gl"],
+        },
+      },
+    },
+  },
 });
